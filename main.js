49e76f95609362f6674d6689ca00174fff73669a
Mark Dewey <mark.t.dewey@gmail.com>
2013-11-23 10:05:03 -0500

    writing to multiple LRSs

--- main.js
+++ main.js
@@ -1,77 +1,94 @@
+function sendStatement(lrs, title, url)
+{
+
+	var email = lrs.email;
+	var endpoint = lrs.endpoint +"/statements";
+	var username = lrs.username;
+	var password = lrs.password;
+
+	var statement = {"actor": {
+						"objectType": "Agent", 
+						"mbox":"mailto:" + lrs.email
+					},
+					"verb" : { 
+						"id":"http://example.com/learned", 
+						"display":{
+							"en-US":"learned"
+						} 
+					},
+					"object":{
+						"id":url,
+						"definition":{
+							"name":{ 
+								"en-US":title 
+							},
+							"description":{ 
+								"en-US":"I did this site." 
+							}
+						}
+					}
+				};
+
+	if (!window.btoa) {
+		window.btoa = function (str) {
+			return Base64.encode(str);
+		}
+	}
+	
+	var encodedPassword = btoa(username+":"+password);
+	
+	$.ajax({
+		type: "POST",
+		url: endpoint,
+		async: true,
+		headers: {"Authorization": "Basic "+encodedPassword, "X-Experience-API-Version" : "1.0.0"},
+		password: password,
+		data: JSON.stringify(statement),
+		contentType:"application/json; charset=utf-8",
+		success: function (){
+			$("#output").hide();
+			$("#success").show();
+			$("#success").append("&nbsp;|&nbsp;");
+		},
+		error: function( jqXHR, textStatus, errorThrown ){
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
+		}
+	});
+}
+
 $(function(){
-	 chrome.tabs.getCurrent(function(tab)
-	{
+	chrome.tabs.getCurrent(function(tab){
 		chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-			 var activeTab = arrayOfTabs[0];
-			 var title = activeTab.title;
-			 var url = activeTab.url;
+			var activeTab = arrayOfTabs[0];
+			var title = activeTab.title;
+			var url = activeTab.url;
+		
 			
-			var allLRss = setAllLRSFromStorage();
-			
-			for (var i = 0; i < lrss.length; i++)
+			var allLRss = getAllLRSFromStorage();
+			console.log(allLRss);
+				
+			for (var i = 0; i < allLRss.length; i++)
 			{
 				//TODO: make this thing
-				sendStatement(lrss[i]);
+				console.log("sending to ");
+				console.log(allLRss[i]);
+				sendStatement(allLRss[i], title, url);
 			}
-			 var lrs = getLRSFromStorage();
-			 
-			 var email = lrs.email;
-			 var endpoint = lrs.endpoint +"/statements";
-		     var username = lrs.username;
-			 var password = lrs.password;
-		
-			 var statement = {
-								"actor": {
-									"objectType": "Agent", 
-									"mbox":"mailto:" + lrs.email
-								},
-								"verb" : { 
-									"id":"http://example.com/learned", 
-									"display":{
-										"en-US":"learned"
-									} 
-								},
-								"object":{
-									"id":url,
-									"definition":{
-										"name":{ 
-											"en-US":title 
-										},
-										"description":{ 
-											"en-US":"I did this site." 
-										}
-									}
-								}
-							};
-			 
-			  if (!window.btoa) {
-				window.btoa = function (str) {
-					return Base64.encode(str);
-				}
-			}
-			var encodedPassword = btoa(username+":"+password);
-			 $.ajax({
-			  type: "POST",
-			  url: endpoint,
-			  async: true,
-			  headers: {"Authorization": "Basic "+encodedPassword},
-			  password: password,
-			  data: JSON.stringify(statement),
-			  contentType:"application/json; charset=utf-8",
-			  success: function (){
-				$("#output").html("Success");
-			  },
-			   error: function( jqXHR, textStatus, errorThrown )
-			   {
-				console.log(jqXHR);
-				console.log(textStatus);
-				console.log(errorThrown);
-			   }
-			});
-			 
+			
 		});
 	});
-	}
-);
+});
+	 
+		
+			
+			
+		
+		
+	
+	
+	
+
 
 
